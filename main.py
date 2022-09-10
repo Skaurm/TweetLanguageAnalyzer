2718b0249b85f865b30c20437bc480a769fd7f5b
Markus Nimi <markus.nimi@mail.utoronto.ca>
2022-09-09 23:28:19 -0400

    Added basic call to tweepy API

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import tweepy
+import config
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
+# client = tweepy.Client(
+#     consumer_key=config.CONSUMER_KEY,
+#     consumer_secret=config.CONSUMER_SECRET,
+#     access_token=config.ACCESS_TOKEN,
+#     access_token_secret=config.ACCESS_TOKEN_SECRET
+# )
+
+query = "e lang:de -has:links -has:mentions -has:media"
+
+# response = client.search_recent_tweets(query=query, max_results=10)
+response = client.search_recent_tweets(query=query, max_results=10, tweet_fields=["lang"], expansions=["author_id"])
+users = {u["id"]: u for u in response.includes["users"]}
+for i in range(0, len(response.data)):
+    print("============== TWEET#" + str(i) + "==============")
+    print(response.data[i].text)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
